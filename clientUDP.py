2dc5f197ade51cfa1a6e38fa305f81fc871ba2b0
Nadia Recarey <nadia.recarey@oktana.io>
2020-10-08 18:22:51 -0300

    telnetServer  y agrega main

--- clientUDP.py
+++ clientUDP.py
@@ -1,10 +1,19 @@
 from socket import *
+import ipaddress
+
+IP = unicode(gethostbyname(gethostname()))
+MASK = unicode('255.255.255.0')
+
 serverPort = 2020
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+net = ipaddress.IPv4Network(IP + '/' + MASK, False)
+print('Broadcast:', net.broadcast_address)
+
 message = raw_input("Escriba una frase en minusculas:")
 # message = "<ANNOUNCE\n<mulan.mkv>\t<11961255187>\t<>"
 print(message)
-clientSocket.sendto(message.encode(),('10.0.1.120', serverPort))
+clientSocket.sendto(message.encode(),(net.broadcast_address, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
