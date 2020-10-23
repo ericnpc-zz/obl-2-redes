7ae46bdb3095cb15be25cd59b5916f4c9dd69c99
Eric Pintos <eric@decemberlabs.com>
2020-10-22 22:47:48 -0300

    Envio Varios Announces

--- announceListener.py
+++ announceListener.py
@@ -4,19 +4,36 @@ def startListening():
 
 	serverPort = 2020
 	serverSocket = socket(AF_INET, SOCK_DGRAM)
-	serverSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+	# serverSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 	serverSocket.bind(('', serverPort))
 	print("El announceListener esta listo para recibir")
 
-	while True:
-		message, clientAddress = serverSocket.recvfrom(2048)
-		fullMessage = ''
-		print('received message: ', message)
-		while message:
-			fullMessage = fullMessage + str(message)
-			message = serverSocket.recvfrom(2048)
+	# announcesInProgress = {
+	# 	ip: [paquetes] # cuando se terminen se descartan - podemos usar seq_numbers y tambien timestamps y timeouts
+	# }
 
-		print(message)
+	storedAnnounces = {}
+
+	def addAnnouncement(ip, payload):
+		if ip in storedAnnounces:
+			announceForIp = storedAnnounces[ip]
+			if not payload in announceForIp:
+				storedAnnounces[ip].append(payload)
+		else:
+			storedAnnounces[ip] = [payload]
+		print(storedAnnounces)
+
+	while True:
+		message, clientAddress = serverSocket.recvfrom(4096)
+		# fullMessage = ''
+		# print('received message: ', message)
+		# while message:
+		# 	fullMessage = fullMessage + str(message)
+		# 	message = serverSocket.recvfrom(4096)
+		# 	print('received message: ', message)
+
+		addAnnouncement(clientAddress, message)
+		#print(message)
 		# handleAnnouncement()
 		# serverSocket.sendto(message.encode(), clientAddress)
 
